ee8bc0a4683046f8a454df9edc984fe97e3bbc0b
Y Pham <phamyqb@gmail.com>
2022-10-27 16:10:44 +0700

    remove lib folder

--- lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { ArrayWithLength, NumericRange } from "./types";
-interface CaptchaOptions {
-    numberOnly?: boolean;
-    length?: NumericRange<ArrayWithLength<3>, 15>;
-}
-declare const generate: (options?: CaptchaOptions) => string;
-export { generate };
