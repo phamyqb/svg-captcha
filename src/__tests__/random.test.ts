989e70ff5365591b65d237d26127f6572810c820
Y Pham <phamyqb@gmail.com>
2022-10-27 17:41:49 +0700

    1.0.1

--- src/__tests__/random.test.ts
+++ src/__tests__/random.test.ts
@@ -3,7 +3,7 @@ import { generate } from '../index';
 test('[generate] default', () => expect(generate()).toHaveLength(3));
 test('[generate] custom length 5', () => expect(generate({ length: 5 })).toHaveLength(5));
 test('[generate] number only', () => {
-    const result = generate({ numberOnly: true, length: 5 });
-    expect(result).toHaveLength(5);
-    expect(result).toMatch(/^[0-9]*$/);
+  const result = generate({ numberOnly: true, length: 5 });
+  expect(result).toHaveLength(5);
+  expect(result).toMatch(/^[0-9]*$/);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,31 @@
-import { ArrayWithLength, NumericRange } from "./types";
+import { ArrayWithLength, NumericRange } from './types';
 
 interface CaptchaOptions {
-    numberOnly?: boolean,
+  numberOnly?: boolean;
 
-    /* @defaultValue 3 */
-    length?: NumericRange<ArrayWithLength<3>, 15>
+  /* @defaultValue 3 */
+  length?: NumericRange<ArrayWithLength<3>, 15>;
 }
 
 const codes: number[][] = [
-    [48, 57], // 0 - 9
-    [65, 90], // A - Z
-    [97, 122] // a - z
+  [48, 57], // 0 - 9
+  [65, 90], // A - Z
+  [97, 122], // a - z
 ];
 
 const randomRange = (min: number, max: number) => Math.floor(Math.random() * (max - min) + min);
 
 const generate = (options?: CaptchaOptions) => {
-    const { numberOnly, length = 3 } = options ?? {};
-    let charCodes = [];
-    if (numberOnly)
-        charCodes = Array.from({ length }, () => randomRange(codes[0][0], codes[0][1]));
-    else {
-        const chars = codes.reduce((arr, [min, max]) => arr.concat(Array.from({ length: (max - min) + 1 }, (_, k) => k + min)), []).sort(() => Math.random() - .5);
-        charCodes = Array.from({ length }, () => chars[randomRange(0, chars.length)]);
-    }
-    return String.fromCharCode(...charCodes);
+  const { numberOnly, length = 3 } = options ?? {};
+  let charCodes = [];
+  if (numberOnly) charCodes = Array.from({ length }, () => randomRange(codes[0][0], codes[0][1]));
+  else {
+    const chars = codes
+      .reduce((arr, [min, max]) => arr.concat(Array.from({ length: max - min + 1 }, (_, k) => k + min)), [])
+      .sort(() => Math.random() - 0.5);
+    charCodes = Array.from({ length }, () => chars[randomRange(0, chars.length)]);
+  }
+  return String.fromCharCode(...charCodes);
 };
 
 export { generate };
